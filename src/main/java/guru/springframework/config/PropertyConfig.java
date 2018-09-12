66c0c321ec1e95ddcedb4f81e89c6fcf9a298ff8
Simon Heyden <simon@family-heyden.net>
2018-09-12 18:44:39 +0200

    example for yaml properties

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -2,24 +2,13 @@ package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-		@PropertySource("classpath:datasource.properties"),
-		@PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
 
-	@Autowired
-	Environment environment;
 
 	@Value("${jdbc.user}")
 	private String user;
@@ -46,7 +35,7 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		final FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(environment.getProperty("USERNAME"));
+		fakeDataSource.setUser(user);
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
